177f31d36401383efa67bcfe770050e63ea7b3fe
tommyBjorkman <tommy.bjorkman@utb.ecutbildning.se>
2023-04-04 13:18:42 +0200

    added customer info to caseentity

--- finalassignment/Models/Entities/CaseEntity.cs
+++ finalassignment/Models/Entities/CaseEntity.cs
@@ -2,7 +2,14 @@
 {
     internal class CaseEntity
     {
+        public int Id { get; set; }
+        public string CustomerName { get; set; } = null!;
+        public string CustomerEmail { get; set; } = null!;
+        public string? CustomerPhone { get; set; }
+        public string? CustomerCompany { get; set; }
         public int StatusId { get; set; }
         public StatusEntity Status { get; set; } = null!;
+        public int UserID { get; set; }
+        public UserEntity User { get; set; } = null!;
     }
 }
---
+++ finalassignment/Models/Entities/UserEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace finalassignment.Models.Entities
+{
+    internal class UserEntity
+    {
+        public ICollection<CaseEntity> Cases { get; set; } = new List<CaseEntity>();
+    }
+}
