536293460c45c612a1b7b6a62c3f478beac2b3a2
tommyBjorkman <tommy.bjorkman@utb.ecutbildning.se>
2023-04-05 23:48:22 +0200

    userServices created and edited

---
+++ finalassignment/Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿using finalassignment.Contexts;
+using finalassignment.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace finalassignment.Services;
+
+internal class UserService
+    {
+        private readonly DataContext _context = new();
+        public async Task CreateAsync(UserEntity entity)
+        {
+            var _entity = await _context.Users.FirstOrDefaultAsync(x  => x.Email == entity.Email);
+            if (_entity == null)
+            {
+                await _context.AddAsync(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    public async Task<UserEntity> GetAsync(Func<UserEntity, bool> predicate)
+    {
+        var _entity = await _context.Users.FindAsync(predicate);
+        return _entity!;
+    }
+    public async Task<IEnumerable<UserEntity>> GetAllAsync()
+    {
+        return await _context.Users.ToListAsync();
+    }
+}
+
